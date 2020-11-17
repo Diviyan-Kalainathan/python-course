5fc02c7b6e68b7fd8ccaf80f48ecb2e6b8f8db54
Diviyan Kalainathan <diviyan@fentech.ai>
2020-11-18 00:16:36 +0100

    [INIT] Initial commit

---
+++ Exercices/Basic/Ex1.py
@@ -0,0 +1,11 @@
+# Exercise 1: Here you have a list called 'ABC', composed of numbers and strings,
+# the goal is to have (programmatically) a list 'result' that only has the strings of ABC.
+#
+
+ABC = [1, 'a', 2334123.23, 'dewfv', '23', 3323]
+
+result = []
+# Fill code here.
+
+print(result)
+# result should be : ['a', 'dewfv', '23']
