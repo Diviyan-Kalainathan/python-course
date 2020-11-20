ccc80dc00717870bf070c2d83465eb7dcae8f69e
Diviyan Kalainathan <diviyan@fentech.ai>
2020-11-20 14:47:49 +0100

    Update Ex7.py

--- Exercices/Basic/Ex7.py
+++ Exercices/Basic/Ex7.py
@@ -1,3 +1,3 @@
-# Ex6: list of even and odd numbers
-# 1. Create a function ´even(x)´ that takes as argument an int ´x´ and outputs a list of all even numbers between 0 and x (not included)
-# 2. Create a function ´odd(x)´ that takes as argument an int ´x´ and outputs a list of all odd numbers between 0 and x (not included)
+# Ex7: multiply each element of a List 
+# 1. Create a function ´multiply(l, x)´ that takes as argument a list 'l' and a float ´x´
+#    and outputs a list of all elements of l that are multiplied by x
