e97daa08822c5c645e6864f5206f13fa474a2d18
Diviyan Kalainathan <diviyan@fentech.ai>
2020-11-20 14:45:21 +0100

    Create Ex7.py

---
+++ Exercices/Basic/Ex7.py
@@ -0,0 +1,3 @@
+# Ex6: list of even and odd numbers
+# 1. Create a function ´even(x)´ that takes as argument an int ´x´ and outputs a list of all even numbers between 0 and x (not included)
+# 2. Create a function ´odd(x)´ that takes as argument an int ´x´ and outputs a list of all odd numbers between 0 and x (not included)
