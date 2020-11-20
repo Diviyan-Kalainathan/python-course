794bbf14aa8e1551cca17c0aed6090abdf8658fa
Diviyan Kalainathan <diviyan@fentech.ai>
2020-11-20 14:40:50 +0100

    Create Ex4.py

---
+++ Exercices/Basic/Ex4.py
@@ -0,0 +1,5 @@
+# Ex4: Conversion of types. Create 3 functions 'tofloat', 'tostr', 'toint': that
+# convert any python variable to respectively a float, a str or an int
+# Note: some of the variables cannot be converted (ex: 'fcsd' cannot be converted into an int.)
+# We will focus on an set of examples that can be converted
+
