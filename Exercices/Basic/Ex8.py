b2e93a5ce7f7c9a549ba981b2f32c25f76a18a55
Diviyan Kalainathan <diviyan@fentech.ai>
2020-11-20 14:55:38 +0100

    Create Ex8.py

---
+++ Exercices/Basic/Ex8.py
@@ -0,0 +1,3 @@
+# Ex8: Creating permutations.
+# create a function `permutation(l)` that takes a list `l` and computes all the possible 2-permutations between its items
+# Ex: permutation([1,2,3]) gives [[1,2],[1,3], [2,1], [2,3], [3,1], [3,2]]
