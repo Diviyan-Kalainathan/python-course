a4d613b3fc007ef1911cb6fa7c5cd419c4c7837d
Diviyan Kalainathan <diviyan@fentech.ai>
2020-12-01 00:18:51 +0100

    Create Ex9.py

---
+++ Exercices/Basic/Ex9.py
@@ -0,0 +1,4 @@
+# Ex9: Create a function mix(a,b,c) that takes as input three lists and mixes them in the following order:
+# given a=[a,a,a,..] b=[b,b,b,...] c=[c,c,c...] : output = [a,b,c,a,b,c,a,b,c,...]
+# 
+# Ex 9.1 : Extend this function to any number of lists given in input 
