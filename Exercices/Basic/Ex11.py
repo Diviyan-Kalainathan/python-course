bb619db4efc1017199181b2195c3ba86846fc525
Diviyan Kalainathan <diviyan@fentech.ai>
2020-12-01 00:23:01 +0100

    Create Ex11.py

---
+++ Exercices/Basic/Ex11.py
@@ -0,0 +1,2 @@
+# Ex11: Pandas: create a dataframe with three identical columns of 10 elements
+# from 0 to 10 in the decreasing order. Compute the square root of the sum of each row in another column
